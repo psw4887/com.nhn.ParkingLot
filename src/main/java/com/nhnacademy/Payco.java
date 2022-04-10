81a29352f94fad5e670ced255cda632cb40357d6
psw4887 <83569822+psw4887@users.noreply.github.com>
2022-04-10 09:54:09 +0900

    리팩토링

--- src/main/java/com/nhnacademy/Payco.java
+++ src/main/java/com/nhnacademy/Payco.java
@@ -2,9 +2,6 @@ package com.nhnacademy;
 
 public class Payco {
     public boolean access(User owner) {
-        if(owner.isPaycoAccount()) {
-            return true;
-        }
-        return false;
+        return owner.isPaycoAccount()
     }
 }
